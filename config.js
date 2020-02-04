ba9844f10d6bbec065db442a5696bed566a92cda
AjayU08 <55486341+AjayU08@users.noreply.github.com>
2020-02-04 08:42:24 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCA_Uyic1SrHFHe2wNpSGWJYn_C2Emlu1w",
+  authDomain: "fir-1-dd307.firebaseapp.com",
+  databaseURL: "https://fir-1-dd307.firebaseio.com",
+  projectId: "fir-1-dd307",
+  storageBucket: "fir-1-dd307.appspot.com",
+  messagingSenderId: "909651776396",
+  appId: "1:909651776396:web:5d9221d363c8e421eb3ca9",
+  measurementId: "G-Z40RRS2KHH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
